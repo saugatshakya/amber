cedd76d16e086112026426e11ada6bffb722dff7
Nischal77 <nischalchand48@gmail.com>
2020-08-10 22:39:33 +0545

    database modules

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,13 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+final FirebaseApp app = FirebaseApp(
+    options: FirebaseOptions(
+  googleAppID: '1:329001543546:android:b0abd8a500d653c0c718c3',
+  apiKey: 'AIzaSyDqh058hJJ209H0h7rQu8rssGmI3HR_YTU',
+  databaseURL: 'https://amber-8776e.firebaseio.com' ,   
+    )); //Firebaseoptions //firebaseapp
+
 void main() {
   runApp(MyApp());
 }
@@ -34,4 +41,24 @@ class MyApp extends StatelessWidget {
           home: Wrapper()),
     );
   }
+}
+
+class Item {
+  String key;
+  String title;
+  String body;
+
+  Item(this.title, this.body);
+
+  Item.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        title = snapshot.value["title"],
+        body = snapshot.value["body"];
+
+  toJson() {
+    return {
+      "title": title,
+      "body": body,
+    };
+  }      
 }
